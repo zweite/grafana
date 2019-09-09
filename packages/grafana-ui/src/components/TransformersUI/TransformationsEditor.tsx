a26379adb391826f36c41fba696011c0880a8f4a
Torkel Ödegaard <torkel@grafana.com>
2019-09-09 22:28:37 +0200

    POC: results quick info

--- packages/grafana-ui/src/components/TransformersUI/TransformationsEditor.tsx
+++ packages/grafana-ui/src/components/TransformersUI/TransformationsEditor.tsx
@@ -45,13 +45,34 @@ export class TransformationsEditor extends React.PureComponent<TransformationsEd
     this.setState({ updateCounter: this.state.updateCounter + 1 });
   };
 
+  getResultQuickInfo(dataFrames: DataFrame[]): JSX.Element {
+    return (
+      <table className="filter-table" style={{ width: 'auto' }}>
+        <tr>
+          <th>Query</th>
+          <th>Name</th>
+          <th>Fields</th>
+          <th>Rows / data points</th>
+        </tr>
+        {dataFrames.map(series => (
+          <tr>
+            <td>{series.refId}</td>
+            <td>{series.name}</td>
+            <td>{series.fields.length}</td>
+            <td>{series.length}</td>
+          </tr>
+        ))}
+      </table>
+    );
+  }
+
   renderTransformationEditors = () => {
     const { transformations, getCurrentData } = this.props;
     const hasTransformations = transformations.length > 0;
     const preTransformData = getCurrentData(false);
 
     if (!hasTransformations) {
-      return undefined;
+      return this.getResultQuickInfo(preTransformData);
     }
 
     const availableTransformers = transformersUIRegistry.list().map(t => {
@@ -63,53 +84,56 @@ export class TransformationsEditor extends React.PureComponent<TransformationsEd
 
     return (
       <>
-        {transformations.map((t, i) => {
-          let editor, input;
-          if (t.id === DataTransformerID.noop) {
-            return (
-              <Select
-                className={css`
-                  margin-bottom: 10px;
-                `}
-                key={`${t.id}-${i}`}
-                options={availableTransformers}
-                placeholder="Select transformation"
-                onChange={v => {
+        {this.getResultQuickInfo(preTransformData)}
+        <div>
+          {transformations.map((t, i) => {
+            let editor, input;
+            if (t.id === DataTransformerID.noop) {
+              return (
+                <Select
+                  className={css`
+                    margin-bottom: 10px;
+                  `}
+                  key={`${t.id}-${i}`}
+                  options={availableTransformers}
+                  placeholder="Select transformation"
+                  onChange={v => {
+                    this.onTransformationChange(i, {
+                      id: v.value as string,
+                      options: {},
+                    });
+                  }}
+                />
+              );
+            }
+            const transformationUI = transformersUIRegistry.getIfExists(t.id);
+            input = transformDataFrame(transformations.slice(0, i), preTransformData);
+
+            if (transformationUI) {
+              editor = React.createElement(transformationUI.component, {
+                options: { ...transformationUI.transformer.defaultOptions, ...t.options },
+                input,
+                onChange: (options: any) => {
                   this.onTransformationChange(i, {
-                    id: v.value as string,
-                    options: {},
+                    id: t.id,
+                    options,
                   });
-                }}
+                },
+              });
+            }
+
+            return (
+              <TransformationRow
+                key={`${t.id}-${i}`}
+                input={input || []}
+                onRemove={() => this.onTransformationRemove(i)}
+                editor={editor}
+                name={transformationUI ? transformationUI.name : ''}
+                description={transformationUI ? transformationUI.description : ''}
               />
             );
-          }
-          const transformationUI = transformersUIRegistry.getIfExists(t.id);
-          input = transformDataFrame(transformations.slice(0, i), preTransformData);
-
-          if (transformationUI) {
-            editor = React.createElement(transformationUI.component, {
-              options: { ...transformationUI.transformer.defaultOptions, ...t.options },
-              input,
-              onChange: (options: any) => {
-                this.onTransformationChange(i, {
-                  id: t.id,
-                  options,
-                });
-              },
-            });
-          }
-
-          return (
-            <TransformationRow
-              key={`${t.id}-${i}`}
-              input={input || []}
-              onRemove={() => this.onTransformationRemove(i)}
-              editor={editor}
-              name={transformationUI ? transformationUI.name : ''}
-              description={transformationUI ? transformationUI.description : ''}
-            />
-          );
-        })}
+          })}
+        </div>
       </>
     );
   };
