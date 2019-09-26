f6c77f9d972d0548f50d7e858c3f8206f4779ed4
zweite <zweite@163.com>
2019-09-26 17:12:08 +0800

    mtg alert rule by series

--- pkg/services/alerting/eval_handler_test.go
+++ pkg/services/alerting/eval_handler_test.go
@@ -24,141 +24,374 @@ func TestAlertingEvaluationHandler(t *testing.T) {
 
 		Convey("Show return triggered with single passing condition", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{&conditionStub{
 					firing: true,
+					matches: []*EvalMatch{
+						{
+							Metric: "abc",
+							Firing: true,
+						},
+					},
 				}},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, true)
-			So(context.ConditionEvals, ShouldEqual, "true = true")
+			So(context.ConditionEvals, ShouldEqual, "abc: true = true")
 		})
 
 		Convey("Show return triggered with single passing condition2", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
-				Conditions: []Condition{&conditionStub{firing: true, operator: "and"}},
+				MatchSerie: 1,
+				Conditions: []Condition{&conditionStub{
+					firing:   true,
+					operator: "and",
+					matches: []*EvalMatch{
+						{
+							Metric:   "abc",
+							Firing:   true,
+							Operator: "and",
+						},
+					},
+				}},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, true)
-			So(context.ConditionEvals, ShouldEqual, "true = true")
-		})
-
-		Convey("Show return false with not passing asdf", func() {
-			context := NewEvalContext(context.TODO(), &Rule{
-				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and", matches: []*EvalMatch{{}, {}}},
-					&conditionStub{firing: false, operator: "and"},
-				},
-			})
-
-			handler.Eval(context)
-			So(context.Firing, ShouldEqual, false)
-			So(context.ConditionEvals, ShouldEqual, "[true AND false] = false")
-		})
-
-		Convey("Show return true if any of the condition is passing with OR operator", func() {
-			context := NewEvalContext(context.TODO(), &Rule{
-				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "or"},
-				},
-			})
-
-			handler.Eval(context)
-			So(context.Firing, ShouldEqual, true)
-			So(context.ConditionEvals, ShouldEqual, "[true OR false] = true")
+			So(context.ConditionEvals, ShouldEqual, "abc: true = true")
 		})
 
 		Convey("Show return false if any of the condition is failing with AND operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "and"},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, false)
-			So(context.ConditionEvals, ShouldEqual, "[true AND false] = false")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [true AND false] = false")
+		})
+
+		Convey("Show return true if any of the condition is passing with OR operator", func() {
+			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
+				Conditions: []Condition{
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
+				},
+			})
+
+			handler.Eval(context)
+			So(context.Firing, ShouldEqual, true)
+			So(context.ConditionEvals, ShouldEqual, "abc1: [true OR false] = true")
 		})
 
 		Convey("Show return true if one condition is failing with nested OR operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "or"},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, true)
-			So(context.ConditionEvals, ShouldEqual, "[[true AND true] OR false] = true")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [[true AND true] OR false] = true")
 		})
 
 		Convey("Show return false if one condition is passing with nested OR operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "and"},
-					&conditionStub{firing: false, operator: "or"},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, false)
-			So(context.ConditionEvals, ShouldEqual, "[[true AND false] OR false] = false")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [[true AND false] OR false] = false")
 		})
 
 		Convey("Show return false if a condition is failing with nested AND operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "and"},
-					&conditionStub{firing: true, operator: "and"},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, false)
-			So(context.ConditionEvals, ShouldEqual, "[[true AND false] AND true] = false")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [[true AND false] AND true] = false")
 		})
 
 		Convey("Show return true if a condition is passing with nested OR operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: true, operator: "and"},
-					&conditionStub{firing: false, operator: "or"},
-					&conditionStub{firing: true, operator: "or"},
+					&conditionStub{
+						firing:   true,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   true,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   true,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, true)
-			So(context.ConditionEvals, ShouldEqual, "[[true OR false] OR true] = true")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [[true OR false] OR true] = true")
 		})
 
 		Convey("Should return false if no condition is firing using OR operator", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{firing: false, operator: "or"},
-					&conditionStub{firing: false, operator: "or"},
-					&conditionStub{firing: false, operator: "or"},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
 			handler.Eval(context)
 			So(context.Firing, ShouldEqual, false)
-			So(context.ConditionEvals, ShouldEqual, "[[false OR false] OR false] = false")
+			So(context.ConditionEvals, ShouldEqual, "abc1: [[false OR false] OR false] = false")
 		})
 
 		Convey("Should retuasdfrn no data if one condition has nodata", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{operator: "or", noData: false},
-					&conditionStub{operator: "or", noData: false},
-					&conditionStub{operator: "or", noData: false},
+					&conditionStub{
+						noData:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Valid:    true,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						noData:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Valid:    true,
+								Operator: "or",
+							},
+						},
+					},
+					&conditionStub{
+						noData:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Metric:   "abc1",
+								Valid:    true,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
@@ -168,8 +401,20 @@ func TestAlertingEvaluationHandler(t *testing.T) {
 
 		Convey("Should return no data if one condition has nodata", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{operator: "and", noData: true},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Valid:    false,
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
 				},
 			})
 
@@ -180,9 +425,32 @@ func TestAlertingEvaluationHandler(t *testing.T) {
 
 		Convey("Should return no data if both conditions have no data and using AND", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{operator: "and", noData: true},
-					&conditionStub{operator: "and", noData: false},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Valid:    false,
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Valid:    true,
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
 				},
 			})
 
@@ -192,9 +460,32 @@ func TestAlertingEvaluationHandler(t *testing.T) {
 
 		Convey("Should not return no data if both conditions have no data and using OR", func() {
 			context := NewEvalContext(context.TODO(), &Rule{
+				MatchSerie: 1,
 				Conditions: []Condition{
-					&conditionStub{operator: "or", noData: true},
-					&conditionStub{operator: "or", noData: false},
+					&conditionStub{
+						firing:   false,
+						operator: "and",
+						matches: []*EvalMatch{
+							{
+								Valid:    false,
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "and",
+							},
+						},
+					},
+					&conditionStub{
+						firing:   false,
+						operator: "or",
+						matches: []*EvalMatch{
+							{
+								Valid:    true,
+								Metric:   "abc1",
+								Firing:   false,
+								Operator: "or",
+							},
+						},
+					},
 				},
 			})
 
